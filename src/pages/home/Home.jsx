b5c9fa41e585bd73923d4dc835f89eed16d23fad
Piyush Gupta <piyushgupta1165@gmail.com>
2024-02-27 15:58:56 +0530

    initial commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,18 @@
+import Layout from '../../components/layout/Layout'
+import HeroSection from '../../components/heroSection/HeroSection'
+import Filter from '../../components/filter/Filter'
+import ProductCard from '../../components/productCard/ProductCard'
+import Testimonial from '../../components/testimonial/Testimonial';
+
+function Home() {
+  return (
+    <Layout>
+      <HeroSection />
+      <Filter />
+      <ProductCard/>
+      <Testimonial />
+    </Layout>
+  )
+}
+
+export default Home
