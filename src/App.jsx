f7028689a2ef239db193200f6358ee0a5c94560c
Piyush Gupta <piyushgupta1165@gmail.com>
2024-02-27 16:57:40 +0530

    added login, signup, modal componennts

--- src/App.jsx
+++ src/App.jsx
@@ -9,21 +9,28 @@ import Order from './pages/order/Order';
 import Cart from './pages/cart/Cart';
 import Dashboard from './pages/admin/dashboard/Dashboard';
 import NoPage from './pages/nopage/NoPage';
-import MyState from "./context/data/myState";
+import MyState from './context/data/myState';
+import Login from './pages/registration/Login';
+import Signup from './pages/registration/SignUp';
+import ProductInfo from "./pages/productInfo/ProductInfo";
 
 function App() {
   return (
     <MyState>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/order" element={<Order/>} />
-        <Route path="/cart" element={<Cart/>} />
-        <Route path="/dashboard" element={<Dashboard/>} />
-        <Route path="/*" element={<NoPage/>} />
-      </Routes>
-    </Router>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/order" element={<Order />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="/signup" element={<Signup/>} />
+          <Route path="/productinfo/:id" element={<ProductInfo />} />
+          <Route path="/*" element={<NoPage />} />
+        </Routes>
+      </Router>
     </MyState>
+
   )
 }
 
