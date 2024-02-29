5ef54e73e202fa2c45b37b99075b12b68ea2c3b2
Piyush Gupta <piyushgupta1165@gmail.com>
2024-02-29 14:07:30 +0530

    added addtocart functionaltiy

--- src/components/productCard/ProductCard.jsx
+++ src/components/productCard/ProductCard.jsx
@@ -56,10 +56,10 @@ function ProductCard() {
                         )
                     })}
                 </div>
-                 <div className=" flex justify-center">
+                 {/* {<div className=" flex justify-center">
                                             <button onClick={()=>addCart()} type="button" className="focus:outline-none text-white bg-pink-600 hover:bg-pink-700 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm w-full  py-2">Add To Cart</button>
 
-                                        </div> 
+                                        </div> } */}
             </div>
         </section >
 
