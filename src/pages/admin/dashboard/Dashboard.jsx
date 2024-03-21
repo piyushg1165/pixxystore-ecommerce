0e2c84fa4c020f29ebdcf2831e32570f858de631
Piyush Gupta <piyushgupta1165@gmail.com>
2024-03-21 18:30:43 +0530

    updated layout of dashboard

--- src/pages/admin/dashboard/Dashboard.jsx
+++ src/pages/admin/dashboard/Dashboard.jsx
@@ -1,12 +1,8 @@
-import  { useContext } from 'react'
-import {FaUserTie } from 'react-icons/fa';
-import myContext from '../../../context/data/myContext';
+
 import Layout from '../../../components/layout/Layout';
 import DashboardTab from './DashboardTab';
 
 function Dashboard() {
-    const context = useContext(myContext)
-    const { mode} = context
   return (
     <Layout>
         <section className="text-gray-600 body-font mt-10 mb-10">
