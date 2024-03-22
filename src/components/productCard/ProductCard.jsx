63ace5a2011a76d6d5ef2eb0426449301ca909ed
Piyush Gupta <piyushgupta1165@gmail.com>
2024-03-22 15:07:30 +0530

    fixed productcard

--- src/components/productCard/ProductCard.jsx
+++ src/components/productCard/ProductCard.jsx
@@ -7,8 +7,8 @@ import { Link } from 'react-router-dom'
 
 function ProductCard() {
     const context = useContext(myContext)
-    const { mode, product ,searchkey, setSearchkey,filterType,setFilterType,
-        filterPrice,setFilterPrice} = context
+    const { mode, product ,searchkey,filterType,
+        filterPrice} = context
 
     const dispatch = useDispatch()
     const cartItems = useSelector((state)=> state.cart);
@@ -39,9 +39,9 @@ function ProductCard() {
                         return (
                             <Link to={`/productinfo/${id}`}  key={index} className="p-4 md:w-1/4  drop-shadow-lg " >
                                 <div  className="h-full border-2 hover:shadow-gray-100 hover:shadow-2xl transition-shadow duration-300 ease-in-out    border-gray-200 border-opacity-60 rounded-2xl overflow-hidden" style={{ backgroundColor: mode === 'dark' ? 'rgb(46 49 55)' : '', color: mode === 'dark' ? 'white' : '', }} >
-                                    <div onClick={()=> window.location.href = `/productinfo/${id}`} className="flex justify-center cursor-pointer" >
+                                    <Link to={ `/productinfo/${id}`} className="flex justify-center cursor-pointer" >
                                         <img className=" rounded-2xl w-full h-80 p-2 hover:scale-110 transition-scale-110  duration-300 ease-in-out" src={imageUrl} alt="blog" />
-                                    </div>
+                                    </Link>
                                     <div className="p-5 border-t-2">
                                         <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1" style={{ color: mode === 'dark' ? 'white' : '', }}>PixxyStore</h2>
                                         <h1 className="title-font text-lg font-medium text-gray-900 mb-3" style={{ color: mode === 'dark' ? 'white' : '', }}>{title}</h1>
