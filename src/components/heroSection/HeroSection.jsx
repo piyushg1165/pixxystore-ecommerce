ef619aa389c1a2aa2b5e8faad1996ce5f791ea7c
Piyush Gupta <piyushgupta1165@gmail.com>
2024-03-24 12:56:30 +0530

    updated herosection

--- src/components/heroSection/HeroSection.jsx
+++ src/components/heroSection/HeroSection.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 function HeroSection() {
   return (
     <div>
-        <img src="https://firebasestorage.googleapis.com/v0/b/pixxystore-53a2c.appspot.com/o/Homepage.jpg?alt=media&token=47a2477d-1a6e-48df-8f25-314c0584fa0b" alt="" />
+        <img src="https://firebasestorage.googleapis.com/v0/b/pixxystore-53a2c.appspot.com/o/WhatsApp%20Image%202024-03-24%20at%2012.51.45%20PM.png?alt=media&token=9e77fc44-b5ca-4940-901a-6f6b9da48023" alt="" />
     </div>
   )
 }
 
-export default HeroSection
+export default HeroSection;
