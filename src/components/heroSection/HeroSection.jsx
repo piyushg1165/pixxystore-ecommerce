a01f108fa248494b9b850a8ac10f47cafb08336e
Piyush Gupta <piyushgupta1165@gmail.com>
2024-03-24 13:04:39 +0530

    changed herosection size

--- src/components/heroSection/HeroSection.jsx
+++ src/components/heroSection/HeroSection.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function HeroSection() {
   return (
     <div>
-        <img src="https://firebasestorage.googleapis.com/v0/b/pixxystore-53a2c.appspot.com/o/WhatsApp%20Image%202024-03-24%20at%2012.51.45%20PM.png?alt=media&token=9e77fc44-b5ca-4940-901a-6f6b9da48023" alt="" />
+        <img className='min-w-full' src="https://firebasestorage.googleapis.com/v0/b/pixxystore-53a2c.appspot.com/o/WhatsApp%20Image%202024-03-24%20at%2012.51.45%20PM.png?alt=media&token=9e77fc44-b5ca-4940-901a-6f6b9da48023" alt="" />
     </div>
   )
 }
